30d790e89a6b142b0db9a478d31796de2260ff47
PABLO_GUDE <pablo.gude@ots-coremain.com>
2020-08-17 10:47:54 +0200

    register page and user service implemented

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formError: string; 
+  user: User = {};
+  private id: string;
+  userForm = new FormGroup({
+    email: new FormControl(''), 
+    password: new FormControl(''), 
+    confirm_password: new FormControl(''),
+    first_name: new FormControl(''), 
+    last_name: new FormControl(''), 
+    avatar: new FormControl('')
+  }); 
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  public formSubmit() {
+    if(
+      !this.userForm.controls.email ||
+      !this.userForm.controls.password
+    ) {
+      return this.formError = "All fields are required";
+    }
+    if(!this.formError) {
+      this.user.email = this.userForm.controls.email.value; 
+      this.user.password = this.userForm.controls.password.value; 
+      this.registerUser();
+    }
+  }
+
+
+  registerUser = () => {
+    this.userService.addUser(this.user).subscribe(res => {
+      console.log(res); 
+    }); ;
+  }
 }
